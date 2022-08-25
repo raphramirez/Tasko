7e6242886c67f680763f1114814df77b8a24fd04
Raph Ramirez <ralph@s2dioapps.com>
2022-08-25 16:58:02 +0800

    Initial Commit

---
+++ Tasko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tasko
+//
+//  Created by Raphael Ramirez on 8/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
