db8a70c7382e1f08b17fde47a67cf46b8c8ba735
Raph Ramirez <ralph@s2dioapps.com>
2022-08-25 19:46:59 +0800

    navigation controllers setup

---
+++ Tasko/Application/TabBar/MainTabController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Tasko
+//
+//  Created by Raphael Ramirez on 8/25/22.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
